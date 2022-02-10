428893c491638e1db1b7eb9502a7b9956e13a1d5
Marco Heyden <marco.heyden@kit.edu>
2022-02-10 11:58:11 +0100

    class RegionalDriftDetector

--- detectors/abstract.py
+++ detectors/abstract.py
@@ -4,7 +4,16 @@ from skmultiflow.drift_detection.base_drift_detector import BaseDriftDetector
 
 
 class DriftDetector(BaseDriftDetector, ABC):
-
     @abstractmethod
     def pre_train(self, data):
-        pass
+        raise NotImplementedError()
+
+
+class RegionalDriftDetector(DriftDetector, ABC):
+    @abstractmethod
+    def find_drift_dimensions(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def plot_drift_dimensions(self):
+        raise NotImplementedError()
